b26de4744408e78d080928eba1b1d4ec0934d242
Mackenzie Boyd <mackenzie@athinkingape.com>
2019-02-05 11:11:50 -0800

    fix empty array call into query_radius

--- utils.py
+++ utils.py
@@ -309,6 +309,9 @@ class TreeModel(object):
         self.neighbour_count = neighbour_count
 
     def get_predictions(self, faces):
+        # query_radius doesn't like getting an emptry array.
+        if len(faces) == 0:
+            return []
 
         results = []
 
